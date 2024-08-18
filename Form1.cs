144d64a754df8eea3265ed68e7b16a5223ffc328
Dixon Geiser <DGEISE2490@my.sullivan.edu>
2024-08-18 13:10:04 -0400

    Step 2 of first You Do It project, designing the Form

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,20 @@ namespace CSC240_08_01_EnterInvoices_DG
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
