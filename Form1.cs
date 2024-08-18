509ffc6432ed997ec0143466c2aaa2d9e2329dc3
Dixon Geiser <DGEISE2490@my.sullivan.edu>
2024-08-18 13:29:59 -0400

    Completion of steps 3 and 4, both of which only had to do with coding, no designing.

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,17 @@
 namespace CSC240_08_01_EnterInvoices_DG
 {
+    using System.IO;
     public partial class Form1 : Form
     {
+        const string DELIM = ",";
+        const string FILENAME =
+            @"C:\CSharp\Chapter14\Invoices.txt";
+        int num;
+        string name;
+        double amount;
+        static FileStream outFile = new
+            FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(outFile);
         public Form1()
         {
             InitializeComponent();
